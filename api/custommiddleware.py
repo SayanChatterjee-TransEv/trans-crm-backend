a5c3eaf5a7fd746e50e111289039916fb8848957
SayanChatterjee-TransEv <transmogrify08@outlook.com>
2024-03-13 12:13:28 +0530

    updated and customer details added

--- api/custommiddleware.py
+++ api/custommiddleware.py
@@ -6,7 +6,7 @@ class AllowOnlyCertainIPsMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-        self.allowed_ips = ['']
+        self.allowed_ips = ['110.227.203.185']
 
     def __call__(self, request):
         server_ip = request.META.get('REMOTE_ADDR')
@@ -15,8 +15,8 @@ class AllowOnlyCertainIPsMiddleware:
         print("Server Ip:",server_ip)
         print("Client IP:", client_ip)  
         
-        if client_ip is not None and client_ip not in self.allowed_ips:
-            return HttpResponseForbidden("You are not allowed to access this resource.")
+       # if client_ip is not None and client_ip not in self.allowed_ips:
+        #    return HttpResponseForbidden("You are not allowed to access this resource.")
         
         response = self.get_response(request)
         return response
